642fba6e2171cca3b9f141120172e4f6821ec00e
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-06-10 22:32:17 +0900

    ファイルの保存に対応

--- siprep.py
+++ siprep.py
@@ -3,14 +3,25 @@
 # # 信号処理シンポジウム分析
 
 # モジュール
+import os
 import pandas as pd
 from sipsymp import SipSymp
 
 # 対象年度の設定
-term = range(2008,2018)
+syear = 2008
+eyear = 2017
 
 # 期間内の論文タイトル
-df = SipSymp.titlesDuring(term)
+path = './siprep{0}_{1}.csv'.format(syear,eyear)
+if os.path.exists(path):
+    print('{0} file exists.'.format(path))
+    df = pd.read_csv(path)
+else:
+    print('{0} file doesn\'t exist.'.format(path))
+    term = range(syear,eyear)
+    df = SipSymp.titlesDuring(term)
+    # CSVへの書き出し
+    df.to_csv(path)
 
 #
 df
